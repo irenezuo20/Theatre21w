122658214460815a8d7e8d6e964a4fb10f709026
irenezuo20 <qiyao.zuo.gr@dartmouth.edu>
2021-01-21 16:15:50 -0500

    avatar adjustment

--- Theatre/Assets/Scripts/AvatarManager.cs
+++ Theatre/Assets/Scripts/AvatarManager.cs
@@ -6,18 +6,20 @@ using Photon.Realtime;
 
 public class AvatarManager : MonoBehaviourPun, Photon.Pun.IPunObservable
 {
-    public GameObject avatar;
+    public GameObject upperBody;
     public GameObject leftHand;
+    public GameObject rightHand;
 
     public Transform playerGlobal;
-    public Transform playerLocal;
+    public Transform eyeLocal;
     public Transform leftLocal;
+    public Transform rightLocal;
 
     private PhotonView PV;
 
     void Start()
     {
-        Debug.Log("I'm instantiated");
+        Debug.Log("AvatarManager Start(): avatar instantiated");
 
         PV = GetComponent<PhotonView>();
 
@@ -26,14 +28,21 @@ public class AvatarManager : MonoBehaviourPun, Photon.Pun.IPunObservable
             Debug.Log("player is mine");
 
             playerGlobal = GameObject.Find("OVRPlayerController").transform;
-            playerLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+            eyeLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
             leftLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/LeftHandAnchor");
+            rightLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/RightHandAnchor");
 
             this.transform.SetParent(playerGlobal);
-            this.transform.localPosition = new Vector3(0f, 0f, 0f);
-
-            avatar.transform.SetParent(playerLocal);
+            upperBody.transform.SetParent(eyeLocal);
             leftHand.transform.SetParent(leftLocal);
+            rightHand.transform.SetParent(rightLocal);
+
+            upperBody.transform.localPosition = new Vector3(0f, 0f, 0f);
+            this.transform.localPosition = new Vector3(0f, 0f, 0f);
+            leftHand.transform.localPosition = new Vector3(0f, 0f, 0f);
+            rightHand.transform.localPosition = new Vector3(0f, 0f, 0f);
+
+
             //GetComponent<Recorder>().enabled = true;
 
         }
@@ -48,19 +57,23 @@ public class AvatarManager : MonoBehaviourPun, Photon.Pun.IPunObservable
         {
             stream.SendNext(playerGlobal.position);
             stream.SendNext(playerGlobal.rotation);
-            stream.SendNext(playerLocal.localPosition);
-            stream.SendNext(playerLocal.localRotation);
+            stream.SendNext(eyeLocal.localPosition);
+            stream.SendNext(eyeLocal.localRotation);
             stream.SendNext(leftLocal.localPosition);
             stream.SendNext(leftLocal.localRotation);
+            stream.SendNext(rightLocal.localPosition);
+            stream.SendNext(rightLocal.localRotation);
         }
         else
         {
             this.transform.position = (Vector3)stream.ReceiveNext();
             this.transform.rotation = (Quaternion)stream.ReceiveNext();
-            avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
-            avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+            upperBody.transform.localPosition = (Vector3)stream.ReceiveNext();
+            upperBody.transform.localRotation = (Quaternion)stream.ReceiveNext();
             leftHand.transform.localPosition = (Vector3)stream.ReceiveNext();
             leftHand.transform.localRotation = (Quaternion)stream.ReceiveNext();
+            rightHand.transform.localPosition = (Vector3)stream.ReceiveNext();
+            rightHand.transform.localRotation = (Quaternion)stream.ReceiveNext();
         }
     }
 }
